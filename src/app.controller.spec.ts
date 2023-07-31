589623bd5b302b37a7b1bbbe717caa8f8dbfef5d
cmc4512 <cmirandacolque@gmail.com>
2023-07-31 19:54:15 -0400

    v1.0.2

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "HOLA MUNDO!"', () => {
+      expect(appController.getHello()).toBe('HOLA MUNDO!');
     });
   });
 });
